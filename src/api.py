af9bfbdab3943b1359f621375015ee1b773dbce2
johncx <johnx@computas.com>
2020-07-15 13:41:29 +0200

    setting up test rig

--- src/api.py
+++ src/api.py
@@ -4,13 +4,37 @@
     root has been established, since it makes it easy to check if the
     application is live.
 """
+import json
+import subprocess
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from flask_socketio import send
+from flask_socketio import emit
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(
+    app,
+    cors_allowed_origins="*",
+    engineio_logger=False,
+)
 
 
-@app.route('/')
-def index():
-    return "Yes, we're up!", 200
+@socketio.on("connect")
+def connect():
+    print("===== client connected =====")
+
+@socketio.on("disconnect")
+def disconnect():
+    print("=== client disconnected ===")
+
+@socketio.on("message")
+def handle_message(message):
+    print("client: " + str(message))
+
+
+@socketio.on("startGame")
+def handle_startGame(data):
+    datadict = json.loads(data)
+    for key, value in datadict.items():
+        print(key, ": ", value)
+
