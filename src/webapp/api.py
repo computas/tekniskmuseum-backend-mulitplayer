4344381c4e953888fb81fa92813c601027c67467
Håkon Osland <hosx@computas.com>
2020-07-16 16:57:40 +0200

    fixing models

--- src/webapp/api.py
+++ src/webapp/api.py
@@ -56,7 +56,7 @@ def handle_joinGame(json_data):
           the game.
     """
     player_id = request.sid
-    game_id = models.check_player2_in_mulitplayer()
+    game_id = models.check_player2_in_mulitplayer(player_id)
     if game_id is not None:
         # Update mulitplayer table by inserting player_id for player_2 and change state of palyer_1 in PIG to "Ready"
         print("GAME ID: " + game_id)
