e9024d0817d0392c14bfec363618c43c778a539e
andrealouisestanderen <anstx@computas.com>
2020-07-16 11:37:17 +0200

    Add MP tables and func

--- src/api.py
+++ src/api.py
@@ -6,6 +6,7 @@
 """
 import json
 import uuid
+import models
 from flask import Flask
 from flask import request
 from flask_socketio import (
@@ -24,6 +25,7 @@ socketio = SocketIO(
     cors_allowed_origins="*",
     engineio_logger=False,
 )
+NUM_GAMES = 3  # This is placed here temporarily(?)
 
 @socketio.on("connect")
 def connect():
@@ -42,8 +44,39 @@ def handle_message(message):
 
 @socketio.on("joinGame")
 def handle_joinGame(json_data):
-    # TODO: implement me
+    """
+        Check if player2 is none in mulitplayer table.
+        * If check is false create new mulitplayer game.
+        * If check is true insert player where player2 is none and start
+          the game.
+    """
+    player_id = request.sid
+    game_id = models.check_player2_in_mulitplayer
+    if game_id is not None:
+        models.update_mulitplayer(player_id, game_id)
+        models.insert_into_player_in_game(player_id, game_id, "Ready")  # State not sure
+        join_room(game_id)
+
+    else:
+        game_id = uuid.uuid4().hex
+        labels = models.get_n_labels(NUM_GAMES)
+        models.insert_into_games(game_id, labels, date)
+        models.insert_into_player_in_game(player_id, game_id, "Waiting")
+        models.insert_into_mulitplayer((player_id, None, game_id))
+        join_room(game_id)
     pass
+
+
+@socketio.on("newRound")
+def handle_newRound(json_data):
+    # TODO: implement me!
+    request.sid
+    data = json.loads(json_data)
+    models.update_player_in_game(data.player_id, data.game_id, "Ready")
+    
+    while models.get_record_from_player_in_game()
+    
+
 
 @socketio.on("classify")
 def handle_classify(json_data):
--- src/models.py
+++ src/models.py
@@ -50,13 +50,16 @@ class PlayerInGame(db.Model):
     """
     token = db.Column(db.NVARCHAR(32), primary_key=True)
     game_id = db.Column(db.NVARCHAR(32), nullable=False)
-    state = db.Column(db.Float, nullable=False)
+    state = db.Column(db.String(32)), nullable=False)
 
 
 class MulitPlayer(db.Model):
     """
-        TESTING
+        Table for storing players who partisipate in the same game.
     """
+    player_1 = db.Column(NVARCHAR(32))
+    player_2 = db.Column(NVARCHAR(32))
+    game_id = db.Column(NVARCHAR(32))
 
 
 class Labels(db.Model):
@@ -79,3 +82,249 @@ def create_tables(app):
         db.create_all()
 
     return True
+
+
+def insert_into_games(game_id, labels, date):
+    """
+        Insert values into Games table.
+
+        Parameters:
+        game_id : random uuid.uuid4().hex
+        labels: list of labels
+        date: datetime.datetime
+    """
+    if (
+        isinstance(game_id, str)
+        and isinstance(labels, str)
+        and isinstance(date, datetime.datetime)
+    ):
+
+        try:
+            game = Games(game_id=game_id, labels=labels, date=date)
+            db.session.add(game)
+            db.session.commit()
+            return True
+        except Exception as e:
+            raise Exception("Could not insert into games :" + str(e))
+    else:
+        raise excp.BadRequest(
+            "game_id has to be string, labels has to be string "
+            "and date has to be datetime.datetime."
+        )
+
+
+def insert_into_scores(name, score, date):
+    """
+        Insert values into Scores table.
+
+        Parameters:
+        name: user name, string
+        score: float
+        date: datetime.date
+    """
+    score_int_or_float = isinstance(score, float) or isinstance(score, int)
+
+    if (
+        isinstance(name, str)
+        and score_int_or_float
+        and isinstance(date, datetime.date)
+    ):
+        try:
+            score = Scores(name=name, score=score, date=date)
+            db.session.add(score)
+            db.session.commit()
+            return True
+        except Exception as e:
+            raise Exception("Could not insert into scores: " + str(e))
+    else:
+        raise excp.BadRequest(
+            "Name has to be string, score can be int or "
+            "float and date has to be datetime.date."
+        )
+
+
+def insert_into_player_in_game(token, game_id, play_time):
+    """
+        Insert values into PlayerInGame table.
+
+        Parameters:
+        token: random uuid.uuid4().hex
+        game_id: random uuid.uuid4().hex
+        play_time: float
+    """
+    if (
+        isinstance(token, str)
+        and isinstance(game_id, str)
+        and isinstance(play_time, float)
+    ):
+        try:
+            player_in_game = PlayerInGame(
+                token=token, game_id=game_id, play_time=play_time
+            )
+            db.session.add(player_in_game)
+            db.session.commit()
+            return True
+        except Exception as e:
+            raise Exception("Could not insert into games: " + str(e))
+    else:
+        raise excp.BadRequest(
+            "Token has to be string, game_id has to be string "
+            "and play time has to be float."
+        )
+
+
+def insert_into_mulitplayer(player_1, player_2, game_id):
+    """
+        Docstring.
+    """
+    if (
+        isinstance(player_1, str)
+        and isinstance(player_2, str)
+        and isinstance(game_id, str)
+    ):
+        try:
+            mulitplayer = MulitPlayer(
+                player_1=player_1, player_2=player_2, game_id=game_id
+            )
+            db.session.add(mulitplayer)
+            db.session.commit()
+            return True
+        except Exception as e:
+            raise Exception("Could not insert into mulitplayer: " + str(e))
+    else:
+        raise excp.BadRequest(
+            "All params has to be string."
+        )
+    
+
+
+def check_player2_in_mulitplayer():
+    """
+        Function to check if player2 is none in database. If none, a player can be added to the game.
+    """
+    game=MulitPlayer.query.filter_by(player_2=None).first()
+    if game is not None:
+        game=game.pop().game_id
+    return game
+
+  
+def get_record_from_game(game_id):
+    """
+        Return the game record with the corresponding game_id.
+    """
+    game = Games.query.get(game_id)
+    if game is None:
+        raise excp.BadRequest("game_id invalid or expired")
+
+    return game
+
+
+def get_record_from_player_in_game(token):
+    """
+        Return the player in game record with the corresponding token.
+    """
+    player_in_game = PlayerInGame.query.get(token)
+    if player_in_game is None:
+        raise excp.BadRequest("Token invalid or expired")
+
+    return player_in_game
+
+
+def get_record_from_player_in_game_by_game_id():
+     """
+        Return the player in game record with the corresponding token.
+    """
+    mp=MulitPlayer.query.filter_by(game_id=game_id).first()
+    player_in_game = PlayerInGame.query.get(token)
+    if player_in_game is None:
+        raise excp.BadRequest("Token invalid or expired")
+
+    return player_in_game
+
+def update_game_for_player(game_id, token, session_num, play_time):
+    """
+        Update game and player_in_game record for the incomming game_id and
+        token with the given parameters.
+    """
+    try:
+        game = Games.query.get(game_id)
+        game.session_num += 1
+        player_in_game = PlayerInGame.query.get(token)
+        player_in_game.play_time = play_time
+        db.session.commit()
+        return True
+    except Exception as e:
+        raise Exception("Could not update game for player: " + str(e))
+
+def update_mulitplayer(player2_id, player1_id, game_id):
+    """
+        Update mulitplayer with player 2's id.
+    """
+    try:
+        mp = MulitPlayer.query.filter_by(game_id=game_id).first()
+        player_1=PlayerInGame.qeury.get(player1_id)
+        player_1.game_state="Ready"
+        mp.player_2=player_id
+        db.session.commit()
+        return True
+    except Exception as e:
+        raise Exception("Could not update mulitplayer for player: " + str(e))
+
+def delete_session_from_game(game_id):
+    """
+        To avoid unecessary data in the database this function is called by
+        the api after a session is finished. The record in games table,
+        connected to the particular game_id, is deleted.
+    """
+    try:
+        game = Games.query.get(game_id)
+        db.session.query(PlayerInGame).filter(
+            PlayerInGame.game_id == game_id
+        ).delete()
+        db.session.delete(game)
+        db.session.commit()
+        return "Record deleted."
+    except AttributeError as e:
+        db.session.rollback()
+        raise AttributeError("Couldn't find game_id: " + str(e))
+
+
+def delete_old_games():
+    """
+        Delete records in games older than one hour.
+    """
+    try:
+        games = (
+            db.session.query(Games)
+            .filter(
+                Games.date
+                < (datetime.datetime.today() - datetime.timedelta(hours=1))
+            )
+            .all()
+        )
+        for game in games:
+            db.session.query(PlayerInGame).filter(
+                PlayerInGame.game_id == game.game_id
+            ).delete()
+            db.session.delete(game)
+
+        db.session.commit()
+        return True
+    except Exception as e:
+        db.session.rollback()
+        raise Exception("Couldn't clean up old game records: " + str(e))
+
+
+def get_n_labels(n):
+    """
+        Reads all rows from database and chooses 3 random labels in a list
+    """
+    try:
+        # read all english labels in database
+        labels = Labels.query.all()
+        english_labels = [str(label.english) for label in labels]
+        random_list = random.sample(english_labels, n)
+        return random_list
+
+    except Exception as e:
+        raise Exception("Could not read Labels table: " + str(e))
