7475a40f0e85672ea540af872803ad10d186dfde
Håkon Osland <hosx@computas.com>
2020-07-23 10:46:22 +0200

    added debug flag so you can use test db

--- startapp.sh
+++ startapp.sh
@@ -1,3 +1,15 @@
+# Parse flags
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -d | --debug)       debug=true;
+                            nworkers=1;
+                            shift ;;
+    esac
+done
+
+if [[ $debug = true ]]; then
+    export DEBUG=true
+fi
 cd src
 clear
 gunicorn --bind=0.0.0.0 --worker-class eventlet -w 1 webapp.api:app
