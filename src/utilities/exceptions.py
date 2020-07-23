e655bd96af8a3cafeb13d6d47cc4d4fd8e42a6fb
Marius Brateng <mahbx@computas.com>
2020-07-23 09:21:18 +0200

    add docstring

--- src/utilities/exceptions.py
+++ src/utilities/exceptions.py
@@ -4,6 +4,10 @@
 
 
 class UserError(Exception):
+    """
+        This error should be raised every time there is a user error, e.g. invalid data formatting. 
+    """
+
     def __init__(self, message="Client error, please correct input data"):
         self.message = message
         super().__init__(self.message)
